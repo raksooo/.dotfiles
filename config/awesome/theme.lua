db3f36c9719962e5c728e78683e62465bc118629
Oskar Nyberg <oskar@oskarnyberg.com>
2016-05-13 14:32:44 +0200

    Small changes.

--- config/awesome/theme.lua
+++ config/awesome/theme.lua
@@ -42,7 +42,7 @@ theme.naughty_border_width = 1
 theme.naughty_border_color = "#268bd2"
 
 theme.systray_icon_spacing = 2
-theme.useless_gap = 20
+theme.useless_gap = 19
 theme.less_useless_gap = 0
 
 theme.icon_theme = nil
