7d4efbcc6d9bd1fa0d7fc17c58d1ea81885ebe1b
Oskar Nyberg <oskar@oskarnyberg.com>
2016-09-26 16:20:55 +0200

    Improved messenger widget

--- config/awesome/statusbar/messengerWidget.lua
+++ config/awesome/statusbar/messengerWidget.lua
@@ -3,12 +3,12 @@ function messengerW.widget()
     messengerWidget = wibox.layout.fixed.horizontal()
     messengerW.text = wibox.widget.textbox()
     messengerWidget:add(messengerW.text)
-    tools.initInterval(messengerW.update, 5, true)
+    tools.initInterval(messengerW.update, 3, true)
     return messengerWidget
 end
 
 function messengerW.update()
-    if messengerWindow ~= nil and messengerWindow:find("Messenger") then
+    if messengerWindow ~= nil and messengerWindow.name:len() > 12 and not messengerWindow.name:find("messenger.com/t/") then
         messengerW.text:set_text(messengerWindow.name)
     else
         messengerW.text:set_text('')
