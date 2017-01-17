689902512430b4924e47a21a26dd2b471f64f4a6
Oskar Nyberg <oskar@oskarnyberg.com>
2017-01-17 19:45:00 +0100

    Preventing clients without class to show an error

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -98,7 +98,7 @@ client.connect_signal("manage", function (c)
 end)
 
 client.connect_signal("focus", function(c)
-    if c.class:lower() == terminal then
+    if c.class ~= nil and c.class:lower() == terminal then
         c.opacity = 0.9
     else
         c.opacity = beautiful.opacity_focus
@@ -106,7 +106,7 @@ client.connect_signal("focus", function(c)
     c.border_color = beautiful.border_focus
 end)
 client.connect_signal("unfocus", function(c)
-    if c.class:lower() == terminal then
+    if c.class ~= nil and c.class:lower() == terminal then
         c.opacity = 0.75
     else
         c.opacity = beautiful.opacity_normal
