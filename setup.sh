bbe8ea92fad78812702600cdeb96779796049de6
Oskar Nyberg <oskar@oskarnyberg.com>
2017-10-10 23:07:34 +0200

    Commented parts of setup script

--- setup.sh
+++ setup.sh
@@ -8,36 +8,36 @@ function link {
 
 sudo -v
 
-link zshrc .zshrc
-link tmux.conf .tmux.conf
-link xinitrc .xinitrc
-link Xmodmap .Xmodmap
-link Xresources/Xresources .Xresources
-link Xresources/Xresources_rofi .Xresources_rofi
-link Xresources/Xresources_quantum .Xresources_theme
-link gitconfig .gitconfig
+#link zshrc .zshrc
+#link tmux.conf .tmux.conf
+#link xinitrc .xinitrc
+#link Xmodmap .Xmodmap
+#link Xresources/Xresources .Xresources
+#link Xresources/Xresources_rofi .Xresources_rofi
+#link Xresources/Xresources_quantum .Xresources_theme
+#link gitconfig .gitconfig
 
-link awesome .config/
-link liquidpromptrc .config/
-link liquidprompt .config/
-link qutebrowser .config/
-link sxhkd .config/
-link pacaur .config/
-link gtk/* .config/
-link user-dirs.dirs .config/
+#link awesome .config/
+#link liquidpromptrc .config/
+#link liquidprompt .config/
+#link qutebrowser .config/
+#link sxhkd .config/
+#link pacaur .config/
+#link gtk/* .config/
+#link user-dirs.dirs .config/
 
-mkdir ~/.config/nvim
-link nvim .config/nvim/init.vim
+#mkdir ~/.config/nvim
+#link nvim .config/nvim/init.vim
 
-sudo ln -sf $DOTFILES/config/gtk/gtk-2.0/gtkrc /etc/gtk-2.0/
-sudo ln -sf $DOTFILES/config/gtk/gtk-3.0/settings.ini /etc/gtk-3.0/
+#sudo ln -sf $DOTFILES/config/gtk/gtk-2.0/gtkrc /etc/gtk-2.0/
+#sudo ln -sf $DOTFILES/config/gtk/gtk-3.0/settings.ini /etc/gtk-3.0/
 
 git clone git@oskarnyberg.com:dotfiles/wallpapers.git ~/.wallpapers
 git clone git@oskarnyberg.com:dotfiles/share.git ~/.local/share/oskar
 git clone git@oskarnyberg.com:dotfiles/bin.git ~/.local/bin
 
 git clone git@oskarnyberg.com:dotfiles/moredotfiles.git ~/.moredotfiles
-if [ -f ~/.moredotfiles/setup.sh ]; then
-  cd ~/.moredotfiles/setup.sh
-fi
+#if [ -f ~/.moredotfiles/setup.sh ]; then
+  #cd ~/.moredotfiles/setup.sh
+#fi
 
